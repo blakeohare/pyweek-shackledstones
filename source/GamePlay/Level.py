30300e0ea49de4cbd5d489b2023a4411a0d0b358
ikr <ikr@5cdd2c64-068c-0410-8899-ac5094b1fea8>
2010-08-23 21:22:29 +0000

    whoops, broke it

--- source/GamePlay/Level.py
+++ source/GamePlay/Level.py
@@ -231,7 +231,7 @@ class Level:
 
 		self.width = int(values['width'])
 		self.height = int(values['height'])
-		self.music = values['music']
+		self.music = values.get('music', None)
 		self.layers = {}
 		for layerName in 'A B C D E F Stairs'.split(' '):
 			content = values.get('Layer' + layerName)
