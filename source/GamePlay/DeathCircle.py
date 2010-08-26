c8d06cf46d1f1bb49ebb64ee826e92cd784630e7
blake <blake@5cdd2c64-068c-0410-8899-ac5094b1fea8>
2010-08-26 07:38:44 +0000

    pulling deathcircle out into its own file

---
+++ source/GamePlay/DeathCircle.py
@@ -0,0 +1,22 @@
+
+class DeathCircle:
+	def __init__(self, x, y, radius, duration, type):
+		self.x = x
+		self.y = y
+		self.r = radius
+		self.time_left = duration
+		self.type = type
+		self.expired = False
+	
+	def update(self):
+		self.time_left -= 1
+		if self.time_left < 0:
+			self.expired = True
+	
+	def touches_sprite(self, sprite):
+		dx = sprite.x - self.x
+		dy = sprite.y - self.y
+		max_distance = self.r + sprite.r
+		if dx ** 2 + dy ** 2 < max_distance ** 2:
+			return True
+		return False
