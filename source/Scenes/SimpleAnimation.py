b3538798544e8e11f7e8eab896d606dc1feb9588
fixception <fixception@5cdd2c64-068c-0410-8899-ac5094b1fea8>
2010-08-28 16:38:21 +0000

    Interrogation scenes updated

--- source/Scenes/SimpleAnimation.py
+++ source/Scenes/SimpleAnimation.py
@@ -23,14 +23,14 @@ class SimpleAnimationScene:
 			x = (4 * self.render_counter) % 384
 			y = self.render_counter % 288
 			sky = get_image('misc/sky')
-			transport = get_image('misc/transport' + str(self.render_counter & 1))
+			transport = get_image('misc/airship' + str(self.render_counter & 1))
 			pod = get_image('misc/escapepod' + str(self.render_counter & 1))
 			screen.blit(sky, (x, y))
 			screen.blit(sky, (x - 384, y))
 			screen.blit(sky, (x, y - 288))
 			screen.blit(sky, (x - 384, y - 288))
 			
-			screen.blit(transport, (200, 50))
-			screen.blit(pod, (220 + 3 *  - self.render_counter, 70 + self.render_counter))
+			screen.blit(transport, (60, 10))
+			screen.blit(pod, (220 + 3 *  - self.render_counter, 100 + self.render_counter))
 			
 		self.render_counter += 1
